49fb824e3649fe7400f2b9a3a595241b6bb82ced
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-07-04 02:10:02 -0400

    Initial commit

---
+++ api/event/[eventId].ts
@@ -0,0 +1,29 @@
+import {NowRequest, NowResponse} from '@vercel/node'
+import got from 'got';
+
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const {eventId} = req.query;
+
+  try {
+    const {groupId} = await got.get(`https://sentry.io/api/0/organizations/wulkano-l0/eventids/${eventId}/`, {
+      headers: {authorization: `Bearer ${process.env.SENTRY_TOKEN}`}
+    }).json();
+
+    res.writeHead(302, {
+      'Location': `/api/issue/${groupId}`
+    });
+    res.end();
+  } catch (error) {
+    if (error.response) {
+      if (error.response.statusCode === 404) {
+        res.json({ready: false});
+      } else {
+        res.status(400);
+        res.json({error: error.response.body});
+      }
+    } else {
+      res.json({error});
+    }
+  }
+}
---
+++ api/issue/[issueId].ts
@@ -0,0 +1,54 @@
+import {NowRequest, NowResponse} from '@vercel/node'
+import got from 'got';
+
+const sentryIssueChecker = (id, shortId) => ({body, pull_request}) => {
+  return !pull_request && body && (
+    body.includes(`SENTRY_ID=${id}`) ||
+    body.includes(`https://sentry.io/organizations/wulkano-l0/issues/${id}/`) ||
+    body.includes(shortId)
+  );
+};
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const {issueId} = req.query;
+
+  try {
+    const {shortId, permalink} = await got.get(`https://sentry.io/api/0/issues/${issueId}/`, {
+      headers: {authorization: `Bearer ${process.env.SENTRY_TOKEN}`}
+    }).json();
+
+    const checkSentryIssue = sentryIssueChecker(issueId, shortId);
+
+    const [issue] = await got.paginate.all(`https://api.github.com/repos/karaggeorge/kap-test-playground/issues?state=all&labels=sentry`, {
+      headers: {
+        accept: 'application/vnd.github.v3+json',
+        authorization: `token ${process.env.GH_TOKEN}`
+      },
+      responseType: 'json',
+      pagination: {
+        filter: checkSentryIssue,
+        countLimit: 1,
+        stackAllItems: false
+      }
+    }) as any;
+
+    if (issue) {
+      res.setHeader('Cache-Control', 'public, max-age=604800, immutable, s-maxage=604800')
+      res.json({
+        issueId,
+        shortId,
+        permalink,
+        ghUrl: issue.html_url
+      });
+    } else {
+      res.json({
+        issueId,
+        shortId,
+        permalink,
+        ghIssueTemplate: `<!---SENTRY_ISSUE_ID=${issueId}--->\n\nSentry Issue: [${shortId}](${permalink})\n\n`
+      });
+    }
+  } catch (error) {
+    res.json({error});
+  }
+}
