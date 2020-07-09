319fdbc1e32889cefbf98e25545ec892b9406930
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-07-09 09:07:03 -0400

    Add some more logic

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,12 +1,34 @@
 import {NowRequest, NowResponse} from '@vercel/node'
 import got from 'got';
 
+const checkIssue = body => {
+  return body && (
+    body.includes(`SENTRY_ID=`) ||
+    body.includes(`https://sentry.io/organizations/wulkano-l0/issues/`)
+  );
+};
+
 export default async (req: NowRequest, res: NowResponse) => {
   console.log(req.headers['x-github-event']);
 
   if (req.headers['x-github-event'] === 'issues') {
-    console.log(req.body);
-    res.end();
+    const {action, issue} = req.body;
+
+    console.log(action);
+
+    if (['opened', 'edited'].includes(action)) {
+      if (!issue.labels.some(label => label.name.toLowerCase() === 'sentry')) {
+        console.log('No label');
+
+        if (checkIssue(issue.body)) {
+          console.log('ADD LABEL PLZ');
+        }
+      } else {
+        console.log('Label');
+      }
+    }
+
+    res.json({action, issue});
   } else {
     res.end();
   }
